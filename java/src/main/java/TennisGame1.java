8506c813171c286584ed4d2735e32dac0df8b7dc
jpestanasalinas <julio.pestana.salinas@gmail.com>
2023-09-02 17:37:25 +0200

    refactor: remove unused field

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,15 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
 
-    private String player1Name;
-    private String player2Name;
-
     private Player player1;
     private Player player2;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
         this.player1 = new Player(player1Name);
         this.player2 = new Player(player2Name);
     }
